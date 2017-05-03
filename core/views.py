66c6d8b5b4efdb1a7e8b3f236d9fa9f7129673c9
Damien <damien@dam.io>
2017-05-03 11:01:57 +0200

    Revert "histogram: take into account single run with only one K/Q"
    This reverts commit 66546893581f85671e447fd0cd0c387e33a58dff.

--- core/views.py
+++ core/views.py
@@ -211,12 +211,9 @@ def result(request, pk):
     # export scores for histogram
     scores = models.ProcessingResult.objects \
             .filter(graph=graph).values_list('param_clusters', 'param_topics', 'crit')
-    if len(scores) < 2:
-        scores = None
-    else:
-        scores = sorted(scores, key=lambda s:s[1])
-        scores = sorted(scores, key=lambda s:s[0])
-        scores = [(k,list(v)) for k,v in itertools.groupby(scores, lambda s:s[0])]
+    scores = sorted(scores, key=lambda s:s[1])
+    scores = sorted(scores, key=lambda s:s[0])
+    scores = [(k,list(v)) for k,v in itertools.groupby(scores, lambda s:s[0])]
 
     return HttpResponse(templates.result(request, graph, result, scores=scores))
 
