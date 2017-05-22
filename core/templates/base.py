f999819c961836199fe3c4130dcf58cc86e562f8
Damien <damien@dam.io>
2017-05-22 17:13:14 +0200

    have a proper base.css file

--- core/templates/base.py
+++ core/templates/base.py
@@ -1,39 +1,5 @@
 from lys import L, render, raw
 
-CSS = L.style / raw("""
-/* hack for bootstrap + vivagraph */
-#_graph > svg, #_loading { width: 100%; height: 700px }
-#_loading { padding: 200px; color: #888 }
-/* hack for bootstrap + firefox + input[file] */
-.form-control {
-    height: auto;
-}
-body { background: #eee }
-.container { background: white; margin-top: 10px; border-radius: 4px }
-.container-fluid { background: white; margin: 10px; border-radius: 4px }
-.histogram > span {
-    background: #3a87ad;
-    height: 20%;
-    display: inline-block;
-    width: 30px;
-    margin-top: auto;
-}
-
-.histogram {
-    height: 50px;
-    display: flex;
-}
-
-.blog-article img {
-    box-shadow: 0 0 35px #cccccc;
-    max-width: 50%;
-    display: block;
-    margin: auto;
-    margin-top: 50px;
-    margin-bottom: 50px;
-}
-""".replace('\n', ' '))
-
 def basic_frame(*content, title=''):
     return render((
         raw('<!DOCTYPE html>'),
@@ -45,7 +11,7 @@ def basic_frame(*content, title=''):
                 L.link(rel='stylesheet', href="/static/css/bootstrap.css"),
                 L.link(rel='stylesheet', href="/static/css/balloon.css"),
                 L.link(rel='stylesheet', href="/static/css/rc-slider.css"),
-                CSS,
+                L.link(rel='stylesheet', href="/static/css/base.css"),
             ),
             L.body / content,
         ),
