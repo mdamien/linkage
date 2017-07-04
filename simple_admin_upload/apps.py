6eb0ae92e6ead2913c54cebbb97db2e2aa32a1a6
Damien <damien@dam.io>
2017-07-04 23:17:56 +0200

    simple-admin-upload is now a python package - yeah !

--- simple_admin_upload/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpleAdminUploadConfig(AppConfig):
-    name = 'simple_admin_upload'
--- simple_admin_upload/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class File(models.Model):
-    content = models.FileField()
-
-    def __str__(self):
-        return self.content.name
