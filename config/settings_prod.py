b30173d92b00752dce56273721db0fd06472e02a
Damien <damien@dam.io>
2017-07-21 11:04:37 +0200

    add secrets_prod.py file

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -1,5 +1,10 @@
 from config.settings import *
 
+try:
+    from secrets_prod import SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET, EMAIL_HOST_PASSWORD
+except ImportError:
+    raise Exception('You need to update secrets_prod.py with your own values')
+
 DEBUG = False
 ALLOWED_HOSTS = ['linkage.fr']
 USE_X_FORWARDED_HOST = True
@@ -12,9 +17,10 @@ RAVEN_CONFIG = {
     'release': COMMIT_HASH,
 }
 
+
 EMAIL_HOST = 'mail.gandi.net'
 EMAIL_HOST_USER = 'no-reply@linkage.fr'
-EMAIL_HOST_PASSWORD = 'no_reply_linkage_fr_password'
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'no-reply@linkage.fr'
@@ -28,7 +34,7 @@ DATABASES = {
 }
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '620338696601-94u7gvh4avrlocro69mq73oudr3n5ar5.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '7SQfgD3rVDFTdQinUxzCTa68'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
 SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_EXTRA_ARGUMENTS = {'access_type': 'offline'}
 SOCIAL_AUTH_GOOGLE_OAUTH2_REQUEST_TOKEN_EXTRA_ARGUMENTS = {'access_type': 'offline'}
 
@@ -38,7 +44,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',  # <--- enable this one
+    'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
@@ -46,7 +52,7 @@ SOCIAL_AUTH_PIPELINE = (
 )
 
 SOCIAL_AUTH_GOOGLE_GMAIL_KEY = '620338696601-94u7gvh4avrlocro69mq73oudr3n5ar5.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_GMAIL_SECRET = '7SQfgD3rVDFTdQinUxzCTa68'
+SOCIAL_AUTH_GOOGLE_GMAIL_SECRET = SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
 SOCIAL_AUTH_GOOGLE_GMAIL_AUTH_EXTRA_ARGUMENTS = {'access_type': 'offline'}
 SOCIAL_AUTH_GOOGLE_GMAIL_REQUEST_TOKEN_EXTRA_ARGUMENTS = {'access_type': 'offline'}
 SOCIAL_AUTH_GOOGLE_GMAIL_SCOPE = [
