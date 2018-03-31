ee9d8eced6d810baf7d932f2a46c8da922f8b8ba
Damien <damien@dam.io>
2018-03-31 15:06:46 +0200

    improve linkage enterprise setup

--- make_enterprise_zip.sh
+++ make_enterprise_zip.sh
@@ -1,20 +1,20 @@
 # zip
 #  - source linkage OK
-#  - lib python (le virtualenv) OK
+#  - lib python (le virtualenv) NOT YET WORKING
 #  - nltlk
 #  - linkage-cpp
 #  - arma.so
-#
-# req: python3, redis, postgres
 
-mkdir linkage_zip
+mkdir linkage_enterprise
 
 echo "Copying python sources..."
-rsync -r --copy-links --exclude=.git --exclude="db.sqlite3" --exclude="node_modules" linkage linkage_zip/
-echo "Copying python libs from$VIRTUAL_ENV..."
-mkdir -p linkage_zip/virtualenv/
-rsync -r --copy-links $VIRTUAL_ENV linkage_zip/virtualenv/
+rsync -r --copy-links --exclude=.git --exclude=".pyc" --exclude="venv/" --exclude="config/*prod*.py"
+--exclude="db.sqlite3" --exclude="node_modules" linkage linkage_enterprise/
+
+# echo "Copying python libs from$VIRTUAL_ENV..."
+# mkdir -p linkage_zip/virtualenv/
+# rsync -r --copy-links $VIRTUAL_ENV linkage_zip/virtualenv/
 
 echo "Copying c++ binary..."
-mkdir -p linkage_zip/repos/linkage-cpp/build/
-rsync -r --copy-links repos/linkage-cpp/build/ linkage_zip/repos/linkage-cpp/build
+mkdir -p linkage_enterprise/repos/linkage-cpp/build/
+rsync -r --copy-links repos/linkage-cpp/build/ linkage_enterprise/repos/linkage-cpp/build
---
+++ setup_enterprise.sh
@@ -0,0 +1,17 @@
+sudo apt-get install python3 python3-dev redis-server postgresql virtualenv libomp5
+
+virtualenv -p python3 venv
+. venv/bin/activate
+pip install -r requirements.txt
+python -c "import nltk;nltk.download('punkt');nltk.download('stopwords')"
+
+# setup DB linkage-entreprise
+sudo su postgres -c 'psql -f doc/init_pgsql.sql'
+python manage.py migrate --settings=config.settings_enterprise
+./manage.py shell --settings=config.settings_enterprise -c "
+from django.contrib.auth.models import User
+user = User.objects.create_superuser('admin', '', 'admin')
+"
+python manage.py loaddata --settings=config.settings_enterprise doc/staff_group.json
+
+echo "now runs linkage/scripts/enterprise_serv and linkage/scripts/enterprise_celery"
